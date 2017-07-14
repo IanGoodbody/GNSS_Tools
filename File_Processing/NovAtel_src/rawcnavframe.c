1c42eb02a9818bacc59b51c477c22aac40260ac1
Ian Goodbody <ian.goodbody@afit.edu>
2017-07-14 16:35:17 -0400

    Began adding rawcnavframe stuff

---
+++ File_Processing/NovAtel_src/rawcnavframe.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include "parseNovAtel.h"
+
+#define RCNFRAME_PRN_O 4
+#define RCNFRAME_PRN_B 4
+
+#define RCNFRAME_FRAMEID_O 8
+#define RCNFRAME_FRAMEID_B 4
